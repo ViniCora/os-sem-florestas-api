87127f84c8fd633fbcd9aeb6d4511223f7b43b1d
Vinicius Coradassi <viniciuscoradassi@hotmail.com>
2021-03-13 14:45:24 -0300

    cors

--- app.js
+++ app.js
@@ -28,9 +28,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
-  cors({
-    origin: 'https://os-sem-floresta-api.herokuapp.com/',
-  })
+  cors()
 );
 
 app.use(atributeRouter);
